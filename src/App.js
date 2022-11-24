5a993a02209d8e9971927f0eb1592499034356df
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-11-24 20:45:42 +0900

    SetUp Files

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className='h-header bg-main w-full'>
       <h1 className='text-h1 text-star font-bold underline'>
-        Hello world!
+        Hello
       </h1>
+      <p className="text-white">loem</p>
     </div>
   );
 }
